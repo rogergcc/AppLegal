8a8319749dfa5e865c9f540d7ae0e5d8d5ace218
Roger Colquehuanca <r.colquehuanca@software3000.net>
2018-10-19 18:00:08 -0500

    Agregar archivos de proyecto.

---
+++ AppLegal/AppLegal.Android/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace AppLegal.Droid
+{
+    [Activity(Label = "AppLegal", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+            //setContentView(R.layout.activity_maps);
+            
+            base.OnCreate(savedInstanceState);
+            //SetContentView(Resource.Layout.activity_maps);
+            global::Xamarin.Forms.Forms.Init(ApplicationContext, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ AppLegal/AppLegal/Models/Zonas.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppLegal.Models
+{
+    public class Zonas
+    {
+        private string ZonaTrabajoId { get => ZonaTrabajoId; set => ZonaTrabajoId = value; }
+        private string Descripcion { get => Descripcion; set => Descripcion = value; }
+        private string UsuarioID { get => UsuarioID; set => UsuarioID = value; }
+        private string Direccion { get => Direccion; set => Direccion = value; }
+        private string Latitud { get => Latitud; set => Latitud = value; }
+        private string Longitud { get => Longitud; set => Longitud = value; }
+        private string Radio { get => Radio; set => Radio = value; }
+        private string DentroZona { get => DentroZona; set => DentroZona = value; }
+        private string Estado { get => Estado; set => Estado = value; }
+
+        public Zonas()
+        {
+        }
+
+        public Zonas(string zonaTrabajoId, string descripcion, string usuarioID, string direccion, string latitud, string longitud, string radio, string dentroZona, string estado)
+        {
+            ZonaTrabajoId = zonaTrabajoId;
+            Descripcion = descripcion;
+            UsuarioID = usuarioID;
+            Direccion = direccion;
+            Latitud = latitud;
+            Longitud = longitud;
+            Radio = radio;
+            DentroZona = dentroZona;
+            Estado = estado;
+        }
+
+        
+    }
+
+}
